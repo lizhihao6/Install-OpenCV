f9e2f245c006ac14542e6f35c704e2d99c9b9df8
lizhihao6 <lizhihao6@outlook.com>
2021-06-24 08:07:21 +0000

    add contribute support

--- Ubuntu/2.3/opencv2_3_1.sh
+++
@@ -1,3 +0,0 @@
-sudo apt-get install libopencv-*
-sudo apt-get install python-opencv
-sudo apt-get install python-numpy
--- Ubuntu/opencv_install.sh
+++ Ubuntu/opencv_install.sh
@@ -24,25 +24,28 @@ set -e
 echo "--- Installing OpenCV" $version
 
 echo "--- Installing Dependencies"
-source dependencies.sh
+#source dependencies.sh
 
 echo "--- Downloading OpenCV" $version
 mkdir -p $dldir
 cd $dldir
 #wget -c -O $downloadfile http://sourceforge.net/projects/opencvlibrary/files/opencv-unix/$version/$downloadfile/download
 wget -c -O $downloadfile https://nchc.dl.sourceforge.net/project/opencvlibrary/4.5.2/OpenCV%204.5.2.zip
+wget -c -O contribute.zip  https://github.com/opencv/opencv_contrib/archive/refs/tags/4.5.2.zip
+
 
 echo "--- Installing OpenCV" $version
 echo $downloadfile | grep ".zip"
 if [ $? -eq 0 ]; then
     unzip $downloadfile
+    unzip contribute.zip
 else
     tar -xvf $downloadfile
 fi
-cd opencv-$version
+cd opencv-opencv*
 mkdir build
 cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_GENERATE_PKGCONFIG=ON -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
+cmake -DOPENCV_EXTRA_MODULES_PATH=../../opencv_contrib-4.5.2/modules/ -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D OPENCV_GENERATE_PKGCONFIG=ON -D WITH_TBB=ON -D BUILD_NEW_PYTHON_SUPPORT=ON -D WITH_V4L=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON ..
 make -j 16
 make install
 sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
